271c87da3a72b44b3c3ab8b3b51620cf39d94b91
JosueCristhoper <josuecristhoper.123@gmail.com>
2026-03-20 13:06:24 +0100

    feat: añadida ruta de saludo dinámico y metadatos de autor

--- main.py
+++ main.py
@@ -4,4 +4,12 @@ app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "Hola desde Azure!!!"}
+    return {"message": "Hola desde Azure!!!", "autor": "Josue"}
+
+@app.get("/saludar/{nombre}")
+def saludar_usuario(nombre: str):
+    return {
+        "mensaje": f"Hola {nombre}, bienvenido a mi API en Azure",
+        "tecnologia": "FastAPI",
+        "estado": "Online"
+    }
